986fb23054a4f05477b0d65bec159eaf8b81f7f8
Jovonka <89811202+Jovonka@users.noreply.github.com>
2025-11-01 14:43:58 -0400

    s

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/JovonkaJohnson-/", // 👈 add trailing slash and match repo name exactly
+  base: "/Haven1.0/",
 })
